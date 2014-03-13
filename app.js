3a16e60899b2189c04b8f77534d97fa2725ad48d
Kevin Whinnery <kevin.whinnery@gmail.com>
2014-03-13 12:41:51 -0700

    adding middleware back in

--- app.js
+++ app.js
@@ -18,17 +18,34 @@ app.post('/raffles/:id', twilio.webhook({
 // Set up admin UI routes, secured with HTTP Basic Auth
 var auth = express.basicAuth(process.env.BASIC_UN, process.env.BASIC_PW);
 
+// Middleware to redirect all non-secure traffic to an SSL endpoint - use
+// Heroku's header to determine if the original request was over SSL or not
+var secure = function(request, response, next) {
+    // Check current environment and Heroku protocol header
+    var prod = process.env.NODE_ENV === 'production',
+        notSecure = request.header('x-forwarded-proto') !== 'https';
+
+    // Redirect to SSL if necessary
+    if (prod && notSecure) {
+        var host = request.header('host'), url = request.url;
+        response.redirect('https://'+ host + url);
+    }
+
+    // Otherwise continue to process the request
+    next();
+};
+
 // Home page, show all raffles
-app.get('/', auth, raffleController.index);
-app.get('/raffles', auth, raffleController.index);
+app.get('/', secure, auth, raffleController.index);
+app.get('/raffles', secure, auth, raffleController.index);
 
 // Create A Raffle
-app.post('/raffles', auth, raffleController.create);
+app.post('/raffles', secure, auth, raffleController.create);
 
 // Display and run the given raffle
-app.get('/raffles/:id', auth, raffleController.show);
+app.get('/raffles/:id', secure, auth, raffleController.show);
 
 // Delete a raffle
-app.delete('/raffles/:id', auth, raffleController.remove);
+app.delete('/raffles/:id', secure, auth, raffleController.remove);
 
 
